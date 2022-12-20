c5d01df70094d4353d9b78e053cd5aec6961607b
polar-bet <pbear8190@gmail.com>
2022-12-20 15:43:46 +0200

    master

---
+++ resources/views/userProfile.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Профіль користувача</title>
+</head>
+<body>
+<h1>Профіль користувача</h1>
+<table>
+    <tr>
+        <td>
+            Ім'я:
+        </td>
+        <td>
+            Олексій
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Прізвище:
+        </td>
+        <td>
+            Олексій
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Група:
+        </td>
+        <td>
+            КН-42
+        </td>
+    </tr>
+</table>
+</body>
+</html>
